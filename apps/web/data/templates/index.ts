9b050d1d370589fab89741c7dda5193ed308c645
Seo Minseok <tyy8282@gurufin.com>
2026-03-06 17:53:13 +0900

    chore: remove unused templates
      - Remove network-topology, order-process, git-branch, microservices,
        zk-snark, dijkstra, tcp-tls, aws-transit-gateway templates
      - Update index.ts to reflect remaining templates

--- apps/web/data/templates/index.ts
+++ apps/web/data/templates/index.ts
@@ -1,20 +1,12 @@
-import { awsTransitGatewayTemplate } from "./aws-transit-gateway";
 import { blockchainTemplate } from "./blockchain";
 import { jwtAuthTemplate } from "./jwt-auth";
 import { httpCycleTemplate } from "./http-cycle";
-import { gitBranchTemplate } from "./git-branch";
-import { orderProcessTemplate } from "./order-process";
-import { networkTopologyTemplate } from "./network-topology";
-import { microservicesTemplate } from "./microservices";
-import { zkSnarkTemplate } from "./zk-snark";
-import { simpleTemplate } from "./simple";
-import { dijkstraTemplate } from "./dijkstra";
 import { redisStreamTemplate } from "./redis-stream";
-import { tcpTlsTemplate } from "./tcp-tls";
 import { oauthTemplate } from "./oauth";
 import { cicdTemplate } from "./cicd";
 import { immuneResponseTemplate } from "./immune-response";
 import { legislationTemplate } from "./legislation";
+import { simpleTemplate } from "./simple";
 
 export interface Template {
   name: string;
@@ -23,21 +15,13 @@ export interface Template {
 }
 
 export const TEMPLATES: Template[] = [
-  awsTransitGatewayTemplate,
   blockchainTemplate,
   jwtAuthTemplate,
   httpCycleTemplate,
-  gitBranchTemplate,
-  orderProcessTemplate,
-  networkTopologyTemplate,
-  microservicesTemplate,
-  zkSnarkTemplate,
-  simpleTemplate,
-  dijkstraTemplate,
   redisStreamTemplate,
-  tcpTlsTemplate,
   oauthTemplate,
   cicdTemplate,
   immuneResponseTemplate,
   legislationTemplate,
+  simpleTemplate,
 ];
